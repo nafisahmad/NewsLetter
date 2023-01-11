4009251a69b31b9a9f1b74af40744b0e6c400766
Nafis Ahmad <42237165+nafisahmad@users.noreply.github.com>
2023-01-11 16:52:27 +0530

    added process.env.PORT for deployment

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post('/failure', function (req, res) {
 });
 
 // To establish the server at port 3000
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('The server is running at port 3000');
 });
 
